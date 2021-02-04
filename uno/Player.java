dfdae15a75c5dcb56f822f8453c424b99b02276b
marshie <minhthetyper@gmail.com>
2021-02-03 19:59:22 -0600

    Added getter and add/remove cards method to the Player Class

--- uno/Player.java
+++ uno/Player.java
@@ -6,8 +6,20 @@ import java.util.*;
 
 public class Player {
     private final ArrayList<Card> hand;
-    
+
     public Player() {
         hand = new ArrayList<Card>();
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void addCard(Card card) {
+        hand.add(card);
+    }
+
+    public void removeCard(Card card) {
+        hand.remove(card);
     }
 }
